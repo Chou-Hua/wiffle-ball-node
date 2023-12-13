34717389a9bbd28e29e284b271e5ea3dae94aaa7
AndyWu <andy_wu@softmobile.com.tw>
2023-12-13 17:27:23 +0800

    feat render status check

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -2,6 +2,16 @@ const express = require('express');
 const playerModel = require('../models/models')
 const app = express();
 
+
+//test
+app.get('/',async(req,res) =>{	
+	try{
+		res.send('Test Render is ok');		
+	}catch(err){
+		res.status(500).send(err);
+	}
+});
+
 //getuser
 app.get('/player',async(req,res) =>{
 	const player = await playerModel.find();	
