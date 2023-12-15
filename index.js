6c016e0841fc619004666b7d1ba27d108a01ae6d
AndyWu <andy_wu@softmobile.com.tw>
2023-12-15 16:11:46 +0800

    feat cors  can local use api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const routes = require("./server/routes/apiRoutes.js");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,6 +22,7 @@ async function connectMongoDB() {
 
 connectMongoDB();
 
+app.use(cors());
 app.use(express.json()); //make sure it comes back as json
 
 app.use((req, res, next) => {
